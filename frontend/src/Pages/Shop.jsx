8f381a0a2d6b4ba21ed5ee22a2e0eeabf92a3963
Ayush Kumar <ayushkumar6064@gmail.com>
2025-08-10 13:49:22 +0530

    fix-error

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular.jsx'
 import Offers from '../Components/Offers/Offers.jsx'
-import NewCollection from '../Components/NewCollection/newCollection.jsx'
+import NewCollection from '../Components/NewCollection/NewCollection.jsx'
 import NewsLetter from '../Components/NewsLetter/NewsLetter.jsx'
 const Shop = () => {
   return (
